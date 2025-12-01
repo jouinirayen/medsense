324fa6fb6eca2945860d5eb439b59d203f188701
jouinirayen <jouinirayen67@gmail.com>
2025-12-01 06:13:07 +0100

    Réorganisation complète du projet + nouveaux fichiers

--- describe_table.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/config.php';
-$pdo = (new config())->getConnexion();
-$stmt = $pdo->query("DESCRIBE rendezvous");
-$columns = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($columns as $col) {
-    echo $col['Field'] . " | " . $col['Type'] . " | " . $col['Null'] . " | " . $col['Key'] . "\n";
-}
-?>
